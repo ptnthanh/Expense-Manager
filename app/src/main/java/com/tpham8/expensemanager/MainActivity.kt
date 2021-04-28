f1ee311859d3339976a587fcdaa99ee4b63478c9
Thanh Pham <phamthanh0705@gmail.com>
2021-04-27 20:25:23 -0400

    Initial commit

---
+++ app/src/main/java/com/tpham8/expensemanager/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tpham8.expensemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tpham8.expensemanager.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/tpham8/expensemanager/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.tpham8.expensemanager.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
