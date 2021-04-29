9cd0faa3dcf707a78f1f8af198dd005f2bed80f5
Thanh Pham <phamthanh0705@gmail.com>
2021-04-28 22:04:44 -0400

    Create fragments

---
+++ app/src/main/java/com/tpham8/expensemanager/ui/InfoFragment.kt
@@ -0,0 +1,17 @@
+package com.tpham8.expensemanager.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tpham8.expensemanager.R
+
+class InfoFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+}
