69bb8a6beab8031b88289c9b79c2516b331df989
Thanh Pham <phamthanh0705@gmail.com>
2021-04-30 15:07:51 -0400

    Finish info screen

--- app/src/main/java/com/tpham8/expensemanager/ui/InfoFragment.kt
+++ app/src/main/java/com/tpham8/expensemanager/ui/InfoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tpham8.expensemanager.BuildConfig
 import com.tpham8.expensemanager.R
 import com.tpham8.expensemanager.databinding.FragmentInfoBinding
 
@@ -12,9 +13,25 @@ class InfoFragment : Fragment() {
 
     private var binding: FragmentInfoBinding? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_info, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val infoFragmentBinding = FragmentInfoBinding.inflate(inflater, container, false)
+        binding = infoFragmentBinding
+        return infoFragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            buildTimeTextView.text = BuildConfig.BUILD_TIME
+            titleTextView.text = resources.getString(R.string.app_name)
+            versionTextView.text = BuildConfig.VERSION_NAME
+            copyrightTextView.text = resources.getString(R.string.copyright)
+        }
+    }
 }
